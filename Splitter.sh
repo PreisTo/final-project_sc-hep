df941b9607fe6131f2ccd40acd0258b62a7393c6
Tobi Preis <tobias.preis@tum.de>
2022-07-16 00:46:16 +0200

    fixed multiprocessing splitter for different amount of events

--- Splitter.sh
+++ Splitter.sh
@@ -7,20 +7,37 @@ for run in {0..9..1}; do {
     path="$PWD/$1${run}"
     file="HIJING_LBF_test_small.out"
     lineNrs=( $(grep -n BEGINNINGOFEVENT "${path}/${file}" | awk 'BEGIN {FS=": "} {print $1}') )
-    restEvents=$((${#lineNrs[*]}%${numberOfProcesses}))
     
-    for (( event=0; event<=$((${#lineNrs[*]}-${restEvents})); event++ )); do {
-        (start=$((${lineNrs[${event}]}+1))
-        end=$((${lineNrs[$((${event}+1))]}-1))
-        sed -n ${start},${end}p ${path}/${file} > ${path}/event_${event}.dat)
-    }; done
-    wait
-    startEvent=$((${#lineNrs[*]}-${restEvents}+1))
-    for (( event=${startEvent}; event<=$((${restEvents}-1)); event++ )); do {
-        (start=$((${lineNrs[${event}]}+1))
-        end=$((${lineNrs[$((${event}+1))]}-1))
-        sed -n ${start},${end}p ${path}/${file} > ${path}/event_${event}.dat)
-    }; done
-    wait
-    sed -n ${lineNrs[-1]},$(sed -n '$=' ${path}/${file})p ${path}/${file} > ${path}/event_$((${#lineNrs[*]}-1)).dat
+    if [[ ${#lineNrs[*]} -ge ${numberOfProcesses} ]]; then {
+        restEvents=$((${#lineNrs[*]}%${numberOfProcesses}));
+    }
+    else {
+        restEvents=0
+    }; fi
+    
+    if [[ restEvents -eq 0 ]]; then {
+        for (( event=0; event<$((${#lineNrs[*]})); event++ )); do {
+            (start=$((${lineNrs[${event}]}+1))
+            end=$((${lineNrs[$((${event}+1))]}-1))
+            sed -n ${start},${end}p ${path}/${file} > ${path}/event_${event}.dat)
+        }; done
+        wait
+        sed -n $((${lineNrs[-1]}+1)),$(sed -n '$=' ${path}/${file})p ${path}/${file} > ${path}/event_$((${#lineNrs[*]}-1)).dat
+    }
+    else {
+        for (( event=0; event<=$((${#lineNrs[*]}-${restEvents})); event++ )); do {
+            (start=$((${lineNrs[${event}]}+1))
+            end=$((${lineNrs[$((${event}+1))]}-1))
+            sed -n ${start},${end}p ${path}/${file} > ${path}/event_${event}.dat)
+        }; done
+        wait
+        startEvent=$((${#lineNrs[*]}-${restEvents}+1))
+        for (( event=${startEvent}; event<=$((${restEvents}-1)); event++ )); do {
+            (start=$((${lineNrs[${event}]}+1))
+            end=$((${lineNrs[$((${event}+1))]}-1))
+            sed -n ${start},${end}p ${path}/${file} > ${path}/event_${event}.dat)
+        }; done
+        wait
+        sed -n $((${lineNrs[-1]}+1)),$(sed -n '$=' ${path}/${file})p ${path}/${file} > ${path}/event_$((${#lineNrs[*]}-1)).dat
+    }; fi
 }; done
