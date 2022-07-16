6228fcf0d2c6b50c595bfebc7ed81ae1e34ab2bc
Tobi Preis <tobias.preis@tum.de>
2022-07-16 23:36:31 +0200

    begin strucutre filtering multiprocessing

--- Filter.sh
+++ Filter.sh
@@ -2,17 +2,42 @@
 
 [[ -d $1 ]] || { echo "Not a directory, exiting"; return 1; }   # check if first input is a directory
 
-for run in {0..9..1}; do {
-    numberOfProcesses=$(nproc)
-    path="$PWD/$1${run}"
-    file="HIJING_LBF_test_small.out"
-    pwd_original=$PWD;
-    event_counter=0
-    cd $path
-    while read eventFile; do {
-        
-        cp $eventFile backup_${event_counter}.dat
-        ((event_counter++))
-    }; done < <(find $path -type f -name "event_*.dat")
-    cd $pwd_original
-}; done
+
+function ActualFiltering {
+    local eventFile;
+    local event_counter=$1
+        while read eventFile; do {
+            cp $eventFile backup_${event_counter}.dat
+            awk '{if ($3 == 0) print $0}' < backup_${event_counter}.dat 1>event_${event_counter}.dat
+            CheckFilteringAndRemove ${event_counter}
+            ((event_counter++))
+        }; done < <(find $path -type f -name "event_*.dat")
+}
+function Filtering {
+    for run in {0..9..1}; do {
+        local numberOfProcesses=$(nproc)
+        local path="$PWD/$1${run}"
+        local file="HIJING_LBF_test_small.out"
+        local pwd_original=$PWD;
+        local event_counter=0
+        cd $path
+        for 
+        cd $pwd_original
+    }; done
+}
+
+function CheckFilteringAndRemove {
+    local testBool=0
+    local line
+    while read line; do {
+        awk '{if ($3 != 0) testBool=1}'
+    if [[ testBool -eq 0 ]]; then {
+        rm backup_$1.dat
+    }
+    else {
+        echo "Warning: Filtering failed for event $1!"
+    }; fi
+    }; done < event_$1.dat
+    }
+
+Filtering $1
