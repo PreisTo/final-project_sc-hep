be0372144e043e9de59c9dd4834c8f22f62e1288
Tobi Preis <tobias.preis@tum.de>
2022-07-16 22:45:19 +0200

    added comments

--- Splitter.sh
+++ Splitter.sh
@@ -1,48 +1,53 @@
 #!/bin/bash
 
-[[ -d $1 ]] || { echo "Not a directory, exiting"; return 1; }
+### TODOs ###
+# add total number of lines + 1 to array to reduce lines of code needed
+#############
+
+
+
+[[ -d $1 ]] || { echo "Not a directory, exiting"; return 1; }   # check if first input is a directory
+
+###
+# Check input syntax
+###
 
 for run in {0..9..1}; do {
     numberOfProcesses=$(nproc)
     path="$PWD/$1${run}"
     file="HIJING_LBF_test_small.out"
-    lineNrs=( $(grep -n BEGINNINGOFEVENT "${path}/${file}" | awk 'BEGIN {FS=": "} {print $1}') )
+    lineNrs=( $(grep -n BEGINNINGOFEVENT "${path}/${file}" | awk 'BEGIN {FS=": "} {print $1}') )    # get the line numbers of the beggining of events
     
-    if [[ ${#lineNrs[*]} -ge ${numberOfProcesses} ]]; then {
-        restEvents=$((${#lineNrs[*]}%${numberOfProcesses}));
+    if [[ ${#lineNrs[*]} -ge ${numberOfProcesses} ]]; then {    # check if the amount of events is bigger than nproc
+        restEvents=$((${#lineNrs[*]}%${numberOfProcesses}));    # get modulo of amount of events % nproc
     }
     else {
-        restEvents=0
+        restEvents=0    # if amount of events is smaller or equal set rest to 0 to prevent double event running
     }; fi
     
     if [[ restEvents -eq 0 ]]; then {
-        for (( event=0; event<$((${#lineNrs[*]}-1)); event++ )); do {
+        for (( event=0; event<$((${#lineNrs[*]}-1)); event++ )); do {   # run extracting for all events except the last one
             (start=$((${lineNrs[${event}]}+1))
             end=$((${lineNrs[$((${event}+1))]}-1))
-            
             sed -n ${start},${end}p ${path}/${file} > ${path}/event_${event}.dat)
         }; done
         wait
-        
-        sed -n $((${lineNrs[-1]}+1)),$(sed -n '$=' ${path}/${file})p ${path}/${file} > ${path}/event_$((${#lineNrs[*]}-1)).dat
+        sed -n $((${lineNrs[-1]}+1)),$(sed -n '$=' ${path}/${file})p ${path}/${file} > ${path}/event_$((${#lineNrs[*]}-1)).dat  # extract for last event
     }
     else {
-        for (( event=0; event<$((${#lineNrs[*]}-${restEvents})); event++ )); do {
+        for (( event=0; event<$((${#lineNrs[*]}-${restEvents})); event++ )); do {   # same as before for the first nproc number of events
             (start=$((${lineNrs[${event}]}+1))
             end=$((${lineNrs[$((${event}+1))]}-1))
-            
             sed -n ${start},${end}p ${path}/${file} > ${path}/event_${event}.dat)
         }; done
         wait
-        startEvent=$((${#lineNrs[*]}-${restEvents}))
-        for (( event=${startEvent}; event<$((${#lineNrs[*]}-2)); event++ )); do {
+        startEvent=$((${#lineNrs[*]}-${restEvents}))    # set next event to the next one to prevent double extracting
+        for (( event=${startEvent}; event<$((${#lineNrs[*]}-2)); event++ )); do {   # and for the rest of the events except the last one
             (start=$((${lineNrs[${event}]}+1))
             end=$((${lineNrs[$((${event}+1))]}-1))
-            
             sed -n ${start},${end}p ${path}/${file} > ${path}/event_${event}.dat)
         }; done
         wait
-        
-        sed -n $((${lineNrs[-1]}+1)),$(sed -n '$=' ${path}/${file})p ${path}/${file} > ${path}/event_$((${#lineNrs[*]}-1)).dat
+        sed -n $((${lineNrs[-1]}+1)),$(sed -n '$=' ${path}/${file})p ${path}/${file} > ${path}/event_$((${#lineNrs[*]}-1)).dat  # and of course the last event
     }; fi
 }; done
