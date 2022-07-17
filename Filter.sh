47fe565d1360ede7361383a73d966988fcaeae12
Tobi Preis <tobias.preis@tum.de>
2022-07-17 08:24:36 +0200

    switched back to no multiprocessing for this

--- Filter.sh
+++ Filter.sh
@@ -3,16 +3,6 @@
 [[ -d $1 ]] || { echo "Not a directory, exiting"; return 1; }   # check if first input is a directory
 
 
-function ActualFiltering {
-    local eventFile;
-    local event_counter=$1
-        while read eventFile; do {
-            cp $eventFile backup_${event_counter}.dat
-            awk '{if ($3 == 0) print $0}' < backup_${event_counter}.dat 1>event_${event_counter}.dat
-            CheckFilteringAndRemove ${event_counter}
-            ((event_counter++))
-        }; done < <(find $path -type f -name "event_*.dat")
-}
 function Filtering {
     for run in {0..9..1}; do {
         local numberOfProcesses=$(nproc)
@@ -21,7 +11,12 @@ function Filtering {
         local pwd_original=$PWD;
         local event_counter=0
         cd $path
-        for 
+        while read eventFile; do {
+            cp $eventFile backup_${event_counter}.dat
+            awk '{if ($3 == 0) print $0}' < backup_${event_counter}.dat 1>event_${event_counter}.dat
+            CheckFilteringAndRemove ${event_counter}
+            ((event_counter++))
+        }; done < <(find $path -type f -name "event_*.dat")
         cd $pwd_original
     }; done
 }
