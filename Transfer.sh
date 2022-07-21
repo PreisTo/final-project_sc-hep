cf39fdab571adb106d0f9c9048b78329ce357cb0
Tobi Preis <tobias.preis@tum.de>
2022-07-21 13:32:48 +0200

    added multiprocessing for Transfer.sh

--- Transfer.sh
+++ Transfer.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+
+
+function Transfer2TTree {
+  [[ -d $1 ]] || { echo "Not a directory, exiting"; return 1; }   # check if first input is a directory
+
+}
+
+
+
+#[[ -d $1 ]] || { echo "Not a directory, exiting"; return 1; }   # check if first input is a directory
+
+NumberOfProcesses=$(nproc)
+
+for (( i = 0; i < 10; i++ )); do {
+  Dir=$PWD/$1$i/
+  if [[ NumberOfProcesses -gt 1 ]]; then {
+    (Transfer2TTree $Dir)
+    (( NumberOfProcesses-- ))
+  }
+  else {
+    (Transfer2TTree $Dir)
+    wait && echo "Waiting"
+    NumberOfProcesses=$(nproc)
+  }; fi
+}; done
+
