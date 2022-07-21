51973effa4d0f4a93f5da2c9271aa62eaf902327
Tobi Preis <tobias.preis@tum.de>
2022-07-21 15:05:13 +0200

    select correct columns

--- importASCIIfileIntoTTree.C
+++ importASCIIfileIntoTTree.C
@@ -3,10 +3,10 @@
 
 int importASCIIfileIntoTTree(const char *filename)
 {
- TFile *file = new TFile("output.root","update"); // open ROOT file named 'output.root', where TTree will be saved
+ TFile *file = new TFile("HIJING_LBF_test_small.root","update"); // open ROOT file named 'output.root', where TTree will be saved
  TTree *tree = new TTree("event","data from ascii file"); // make the new TTree for each event
 
- Long64_t nlines = tree->ReadFile(filename,"px:py:pz:E"); // whatever you specify here, will be relevant when you start later reading the TTree branches
+ Long64_t nlines = tree->ReadFile(filename,":PID:::px:py:pz:E"); // whatever you specify here, will be relevant when you start later reading the TTree branches
  tree->Write(); // save TTree to 'output.root' file
  file->Close();
 
