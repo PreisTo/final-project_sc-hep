6cf1e89890293d8cfd332e9167e02183b8dfae23
Tobi Preis <tobias.preis@tum.de>
2022-07-21 15:04:56 +0200

    cleaner now also removing .root files

--- cleaner.sh
+++ cleaner.sh
@@ -5,6 +5,8 @@ for dir in {0..9..1}; do {
     for file in {0..9..1}; do {
         rm $PWD/$1$dir/event_${file}.dat
         remove_backups=$(ls $PWD/$1$dir/ | grep -E 'backup_..dat')
-        rm $PWD/$1$dir/$remove_backups 
+        rm $PWD/$1$dir/$remove_backups
+
     }; done 2> /dev/null
+    rm $PWD/$1$dir/HIJING_LBF_test_small.root
 }; done
