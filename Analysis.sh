0006fe4b2bc5746cd51fee16366bde8735c92a52
Tobi Preis <tobias.preis@tum.de>
2022-07-23 17:27:50 +0200

    finished Analysis.sh

--- Analysis.sh
+++ Analysis.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+function ExceAnalyser {
+    local Dir=$1;
+    local Run=$2;
+    echo "Analysing output of Run ${Run}"
+    root -l -b -q readDataFromTTree.C\(\"${Dir}${Run}/HIJING_LBF_test_small.root\"\)
+    return 0;
+}
+
+
 [[ -d $1 ]] || return 1;
+[[ ! -f "AnalysisResults.root" ]] || { rm "AnalysisResults.root"; echo "Removing old Analysis file"; }
+root -l -b -q createHistos.C\(\"AnalysisResults.root\"\)
 
-for (( Run = 0; Run < 1; Run++ )); do ## TODO change run number!!!
+NumberOfProcesses=$(nproc)
 
-    root -l -b -q readDataFromTTree.C\(\"${1}HIJING_LBF_test_small.root\"\)
-
-done
+for (( Run = 0; Run < 10; Run++ )); do ## TODO change run number!!!
+{
+    ExceAnalyser $1 ${Run}
+}; done
+wait;
+mkdir -p figures
+root -l -b -q exportHistos.C\(\"AnalysisResults.root\"\)
