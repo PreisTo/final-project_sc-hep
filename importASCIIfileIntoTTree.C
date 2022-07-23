c2195f39b5bb7c601a375837b74d2a7282caaf29
Tobi Preis <tobias.preis@tum.de>
2022-07-23 17:28:30 +0200

    fixed import into TTree

--- importASCIIfileIntoTTree.C
+++ importASCIIfileIntoTTree.C
@@ -3,7 +3,7 @@
 
 int importASCIIfileIntoTTree(const char *filename)
 {
- TFile *file = new TFile("HIJING_LBF_test_small.root","update"); // open ROOT file named 'output.root', where TTree will be saved
+ TFile *file = new TFile("HIJING_LBF_test_small.root","update"); // open ROOT file 
  TTree *tree = new TTree("event","data from ascii file"); // make the new TTree for each event
 
  Long64_t nlines = tree->ReadFile(filename,":PID:::px:py:pz:E"); // whatever you specify here, will be relevant when you start later reading the TTree branches
