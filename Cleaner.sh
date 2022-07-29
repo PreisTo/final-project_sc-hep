8f9a330f5ce719b6b7e5c11c3562995889fe72fd
Tobi Preis <tobias.preis@tum.de>
2022-07-29 08:01:08 +0200

    updated cleaner to remove log files

--- Cleaner.sh
+++ Cleaner.sh
@@ -2,6 +2,7 @@
 rm $PWD/$1event_{0..9999..1}.dat 2> /dev/null
 rm $PWD/$1backup_{0..9999..1}.dat 2> /dev/null
 for dir in {0..9..1}; do {
+    rm $PWD/$1$dir/log
     for file in {0..9..1}; do {
         rm $PWD/$1$dir/event_${file}.dat
         remove_backups=$(ls $PWD/$1$dir/ | grep -E 'backup_..dat')
