d67274fe28441a9e113c63312d713cdedc1f8ef2
Tobi Preis <tobias.preis@tum.de>
2022-07-29 08:02:55 +0200

    reformatting

--- TheFinalTouch.sh
+++ TheFinalTouch.sh
@@ -4,9 +4,10 @@
 
 [[ ! -d $1 ]] && echo "Not a valid directory!" && return 6
 
-time source Splitter.sh $1 && echo "Done with Splitter.sh" || return 5
-time source Filter.sh $1 && echo "Done with Filter.sh" || return 4
-time source Transfer.sh $1 && echo "Done with Transfer.sh" || return 3
-time source Analysis.sh $1 && echo "Done with Analysis.sh" || return 2
+source Splitter.sh $1 && echo "Done with Splitter.sh" || return 5
+source Filter.sh $1 && echo "Done with Filter.sh" || return 4
+source Transfer.sh $1 && echo "Done with Transfer.sh" || return 3
+source Analysis.sh $1 && echo "Done with Analysis.sh" || return 2
+
 
 return 0;
